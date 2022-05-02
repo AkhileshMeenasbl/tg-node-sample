9310149cf2ecc722e12ef2690ee47bd298acaeed
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-02 22:16:56 +0530

    update web.js

--- web.js
+++ web.js
@@ -8,24 +8,26 @@ var app = express();
 
 app.use(express.json());
 app.use(express.static("express"));
+
+//METHOD 2
 // default URL for website
 app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/Public/simple.html'));
     //__dirname : It will resolve to your project folder.
   });
-const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
-console.debug('Server listening on port ' + port);
+//const server = http.createServer(app);
+//const port = 3000;
+//server.listen(port);
+//console.debug('Server listening on port ' + port);
 
+//METHOD 1
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
 
-//app.get('/', function (req, res) {
-//  res.json({ version: packageInfo.version });
-//});
+var server = app.listen(process.env.PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-//var server = app.listen(process.env.PORT, function () {
-//  var host = server.address().address;
-//  var port = server.address().port;
-
-//  console.log('Web server started at http://%s:%s', host, port);
+  console.log('Web server started at http://%s:%s', host, port);
 //});
