1b102a6357974a24c1f909ca26520eb1b1e127a1
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-02 14:14:34 +0530

    update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,39 @@ const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '5371616713:AAHEIGIg8eoxKXs66GJfK3xfcMFKTargGxs';
 const bot = new TelegramBot(TOKEN, {polling: true});
 
+// Variable
+const Welcome = "Hi i am Akhil's Bot."
+
 // YOUR CODE STARTS HERE
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id,
-  "Hey, I'm a brand new Telegram bot. I live inside a Sanity tutorial.");
-  });
+bot.onText(/\/start/,(msg) => {
+  bot.sendMessage(msg.chat.id, Welcome,
+  {
+    reply_markup : {
+      inline_keyboard : [[
+        {
+          text : "Akhil",
+          web_app : {url : ""}
+        }
+        ]]
+    }
+  }
+  );
+});
+
+
+//bot.onText(/\/start/, (msg) => {
+//  bot.sendMessage(msg.chat.id,
+//  "Hey, I'm a brand new Telegram bot. I live inside a Sanity tutorial.");
+//  {
+//    reply_markup : {
+//      inline_keyboard : [[
+//        {
+//          text : "Open Demo 🤣",
+//          web_app : {url : "https://webappcontent.telegram.org/demo"}
+//        }
+//        ]],
+//    }
+//  }
+//  });
 
