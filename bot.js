8cdcab61f7b0d8bc86dc7ba01acc4f7a93b015b6
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-02 12:55:52 +0530

    update bot.js

--- bot.js
+++ bot.js
@@ -2,23 +2,7 @@
 
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '5371616713:AAHEIGIg8eoxKXs66GJfK3xfcMFKTargGxs';
-//const bot = new TelegramBot(TOKEN, {polling: true});
-
-//Set Web-Hook
-//const TelegramBot = require('../..');
-const options = {
-  webHook: {
-    //port: process.env.PORT
-    //port: process.env.PORT || 8080
-    port: process.env.PORT || 443
-  }
-};
-const url = process.env.APP_URL || 'https://tg-node-sample.herokuapp.com';
-const bot = new TelegramBot(TOKEN, options);
-
-// This informs the Telegram servers of the new webhook.
-// Note: we do not need to pass in the cert, as it already provided
-bot.setWebHook(`${url}/bot${TOKEN}`);
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 // YOUR CODE STARTS HERE
 
