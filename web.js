42774ba6b1f2b168b725ac03d442c8f3cede8ea1
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-02 22:05:31 +0530

    update web.js

--- web.js
+++ web.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var packageInfo = require('./package.json');
+const http = require('http');
+const path = require('path');
 
 var app = express();
+
+
+app.use(express.json());
+app.use(express.static("express"));
+// default URL for website
+app.use('/app', function(req,res){
+    res.sendFile(path.join(__dirname+'/Public/simple.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+console.debug('Server listening on port ' + port);
+
 
 app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
