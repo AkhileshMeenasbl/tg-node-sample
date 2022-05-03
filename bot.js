a99964afbc04f8bac07dd033c0b0827bf83ec72b
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-03 07:20:32 +0530

    update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,8 @@ bot.onText(/\/start/,(msg) => {
           web_app : {url : "https://tg-node-sample.herokuapp.com/original"}
         },{
           text : "Testing 🙈",
-          web_app : {url : "https://tg-node-sample.herokuapp.com/"}
+          //web_app : {url : "https://tg-node-sample.herokuapp.com/"}
+          web_app : {url : "https://www.careerlauncher.com/upsc/prelims-map-series/"}
         }
         ]]
     }
